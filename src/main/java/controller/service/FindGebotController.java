72caf945f6d8ad17df6668a3e800161bbbb2ee90
Dennis Willers <d.willers@gmx.net>
2018-10-04 18:13:44 +0200

    Struktur aufsetzen

---
+++ src/main/java/controller/service/FindGebotController.java
@@ -0,0 +1,10 @@
+package main.java.controller.service;
+
+/**
+ * Von Dennis Willers (A13A316) am 04.10.2018 erstellt
+ */
+public class FindGebotController {
+    public String getGebotText(){
+        return null;
+    }
+}
---
+++ src/main/java/view/Text.java
@@ -0,0 +1,8 @@
+package main.java.view;
+
+/**
+ * Von Dennis Willers (A13A316) am 04.10.2018 erstellt
+ */
+public interface Text {
+
+}
