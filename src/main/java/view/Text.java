785115b7e430fab84f070b2ab3cd6a7761ec6593
Tim Leuschner <tim.leuschner@gmail.com>
2018-10-04 18:32:19 +0200

    Gebote hinzugefügt

--- src/main/java/view/Text.java
+++ src/main/java/view/Text.java
@@ -4,5 +4,14 @@ package main.java.view;
  * Von Dennis Willers (A13A316) am 04.10.2018 erstellt
  */
 public interface Text {
-
+    String[] ZEHN_GEBOTE = {"Ich bin der Herr, dein Gott. Du sollst nicht andere Götter haben neben mir.",
+                            "Du sollst den Namen des Herrn, deines Gottes, nicht unnütz gebrauchen; denn der Herr wird den nicht ungestraft lassen, der seinen Namen mißbraucht.",
+                            "Du sollst den Feiertag heiligen.",
+                            "Du sollst deinen Vater und deine Mutter ehren, auf daß dir's wohlgehe und du lange lebest auf Erden.",
+                            "Du sollst nicht töten.",
+                            "Du sollst nicht ehebrechen.",
+                            "Du sollst nicht stehlen.",
+                            "Du sollst nicht falsch Zeugnis reden wider deinen Nächsten.",
+                            "Du sollst nicht begehren deines Nächsten Haus.",
+                            "Du sollst nicht begehren deines Nächsten Weib, Knecht, Magd, Vieh noch alles, was sein ist."};
 }
