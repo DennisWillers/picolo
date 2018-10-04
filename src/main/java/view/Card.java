dccdf489fc5a3f378a21b033ca9821af34168a1f
Tim Leuschner <tim.leuschner@gmail.com>
2018-10-04 18:45:25 +0200

    Revert "GebotController intitialisiert"
    This reverts commit ec6d47e86b2f47a21ca6ef306e4389f1f86bbeeb.

--- src/main/java/view/Card.java
+++ src/main/java/view/Card.java
@@ -5,4 +5,16 @@ package main.java.view;
  */
 public interface Card {
     String TITEL = "10-GEBOTE";
+
+    //Titel eines einzelnen Gebotes
+    String GEBOT_1 = "1. Gebot";
+    String GEBOT_2 = "2. Gebot";
+    String GEBOT_3 = "3. Gebot";
+    String GEBOT_4 = "4. Gebot";
+    String GEBOT_5 = "5. Gebot";
+    String GEBOT_6 = "6. Gebot";
+    String GEBOT_7 = "7. Gebot";
+    String GEBOT_8 = "8. Gebot";
+    String GEBOT_9 = "9. Gebot";
+    String GEBOT_10 = "10. Gebot";
 }
